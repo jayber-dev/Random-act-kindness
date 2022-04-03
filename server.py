463c020ae1bc24f8f76c04db1b3687a6f350c9cd
jayber <jayber1@gmail.com>
2022-04-03 20:28:59 +0300

    work on personal computer

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
 if "__main__" == __name__:
     app.run(debug=True)
